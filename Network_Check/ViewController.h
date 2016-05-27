a91053b91ef0e2756b441fb0505c7e7ecce2fcf3
shiyonghui <316643741@qq.com>
2016-05-27 11:09:49 +0800

    Initial Commit

---
+++ Network_Check/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Network_Check
+//
+//  Created by 施永辉 on 16/5/27.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Network_Check/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Network_Check
+//
+//  Created by 施永辉 on 16/5/27.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
