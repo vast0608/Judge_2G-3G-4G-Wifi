d177aeddeaf90ebb39d22c24a1b5e406f7150e05
李晓璐 <vast0608@163.com>
2017-06-21 10:56:51 +0800

    Judge_2G/3G/4G/Wifi

--- Network_Check/ViewController.m
+++ Network_Check/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
-
+#import "Reachability.h"
 @interface ViewController ()
-
+{
+    UIAlertController * ac;
+}
 @end
 
 @implementation ViewController
@@ -19,9 +21,40 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)Click:(id)sender {
+    Reachability * reach = [Reachability reachabilityForInternetConnection];
+    NSString * tips = @"";
+    switch (reach.currentReachabilityStatus)
+    {
+        case NotReachable:
+            tips = @"无网络连接";
+            break;
+            
+        case ReachableViaWiFi:
+            tips = @"Wifi";
+            break;
+            
+        case ReachableViaWWAN:
+            NSLog(@"移动流量");
+        case kReachableVia2G:
+            tips = @"2G";
+            break;
+            
+        case kReachableVia3G:
+            tips = @"3G";
+            break;
+            
+        case kReachableVia4G:
+            tips = @"4G";
+            break;
+    }
+    ac = [UIAlertController alertControllerWithTitle:@"提示" message:[NSString stringWithFormat:@"当前网络状态为:%@",tips] preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction * aa = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [ac dismissViewControllerAnimated:YES completion:nil];
+        ac = nil;
+    }];
+    [ac addAction:aa];
+    [self presentViewController:ac animated:YES completion:nil];
 }
 
 @end
